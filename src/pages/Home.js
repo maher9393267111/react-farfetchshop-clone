73d5ae19ff19ad90ceae1d4b696e04dfe2478882
maher <gomemahero.com>
2022-06-06 11:37:35 +0300

    end nav dropdown lists

---
+++ src/pages/Home.js
@@ -0,0 +1,159 @@
+import React from "react";
+// import categories  from "./data";
+import {arr,categories,arr2} from "./data";
+import { useState,useEffect } from "react";
+const Home = () => {
+
+
+console.log(categories);
+    const [show, setShow] = useState(false);
+    const [index, setIndex] = useState(3);
+    const [left, setLeft] = useState(`left-[${index * 12 + 'px'}]` );
+  
+
+    const [submenu, setSubmenu] = useState([]);
+    const [list, setList] = useState([]);
+    const [active, setActive] = useState(false);
+    const [activeSub, setActiveSub] = useState(false);
+    const [activeList, setActiveList] = useState(false);
+  
+
+//     const indexlefthandler = (index) => {
+// setLeft(index+12);
+// console.log(left,'left');
+
+//     }
+
+  return (
+
+    <div>
+
+  
+    <div className=" flex gap-4    font-bold mt-8 ml-12 relative  z-20">
+        {categories .map((cat,index) => (
+          
+          <div className="  group ">
+<div 
+
+// onMouseDown={ indexlefthandler}
+className="  ">
+
+
+
+{cat?.name}
+
+
+</div>
+
+
+{/* --drop down when hover- */}
+
+
+<div className={`h-0 absolute min-w-[100%]   ${ index > 0 ? ` left-${index} ` : " "}  bottom-[-44px]  
+    group-hover:min-h-[89vh]
+        
+border-gray-600  transition-all duration-100 w-[88%] mr-12 top-[29px]   shadow-2xl   bg-white   `}>
+
+
+{/* --content- */}
+
+
+<div className={` group-hover:h-auto transition-all duration-150 hidden group-hover:block mt-[-6px] inside-it  b h-0  pl-12  pt-5      pb-12`}>
+
+
+{/* -list sub- */}
+
+{/* -------- */}
+
+{/* --grid list and cat image- */}
+<div className=" grid  grid-cols-12">
+
+
+
+
+
+
+<div className="flex gap-12 justify-between  col-span-7  mr-12">
+
+
+{cat?.submenu?.map((sub,index) => (
+    
+<div>
+
+<div className=" mt-4text-xl">
+{sub?.name}
+</div>
+
+{/* -sub list-- */}
+
+<div>
+
+    <ul className=" text-left">
+    {sub?.list?.map((item,index) => (
+
+<li  className=" mb-4  first:mt-6 font-thin">{item}</li>
+
+    ))}
+
+
+    </ul>
+</div>
+
+
+</div>
+
+
+
+))}
+
+</div>
+
+
+<div className=" col-span-4">
+
+<div>
+{/* -----image- */}
+
+<div>
+    <img src={cat.image} alt="" />
+</div>
+
+
+</div>
+
+
+</div>
+
+</div>
+{/* ---------- */}
+
+
+{/* ----right side image */}
+
+
+
+
+
+
+</div>
+
+
+
+
+</div>
+
+
+          </div>
+        ))}
+     
+    </div>
+    dsdsdfgfffffffffffffffffffffffffffffffffff
+    <h2>dsdsgggggggggggggggggggggggggggd</h2>
+    <h2>dsdsd</h2>
+    </div>
+
+
+  );
+};
+
+export default Home;
---
+++ src/pages/data.js
@@ -0,0 +1,229 @@
+export const categories = [
+  {
+    id: 1,
+    name: "sale",
+    image:'https://cdn-static.farfetch-contents.com/content/images/Home_CMS/Cms/01-06-2022/MW/01-06-MW-ACCESSORIES-ALL_ACCESSORIES.jpg',
+    submenu: [
+      {
+        name: "All sale",
+        list: [
+          "Sale coats",
+
+          " Sale sweaters",
+
+          "Sale jackets",
+
+          " Sale t-shirts",
+
+          "Sale shoes",
+
+          "Sale trainers",
+
+          "Sale bags",
+
+          " Sale accessories",
+        ],
+      },
+
+      {
+        name: "style",
+        list: [
+          "Sale: classic",
+
+          "Sale: cult style",
+
+          "Sale: hype streetwear",
+
+          "Sale: minimalism",
+        ],
+      },
+      {
+        name: "editors",
+        list: ["New in to sale", "Best of sale", "Favourite designers on sale"],
+      },
+    ],
+  },
+
+  // -------2 ------
+
+  {
+    id: 2,
+    name: "new In",
+   
+    image:'https://cdn-static.farfetch-contents.com/content/images/Home_CMS/Cms/01-06-2022/MW/01-06-MW-NEW_IN-FESTIVAL.jpg',
+    submenu: [
+      {
+        name: "what is new",
+        list: [
+          "New: classic",
+
+          "New: cult style",
+
+          "New: hype streetwear",
+
+          "New: minimalism",
+        ],
+      },
+
+      {
+        name: "trending now",
+        list: [
+          "  67 new sunglasses",
+
+          "Festival style",
+
+          "83 summer shirts",
+
+          "The loafer ed",
+        ],
+      },
+
+      {
+        name: "discover",
+        list: [
+          "Best of the new season",
+
+          "Exclusives & collabs",
+
+          "25 new names to know",
+        ],
+      },
+    ],
+  },
+
+  // -------3 ------
+  {
+    id: 3,
+    name: "shob By",
+    image:'https://cdn-static.farfetch-contents.com/content/images/Home_CMS/Cms/25-05-2022/MW/25-05-MW-SHOP_BY-MOST_WANTED.jpg',
+    submenu: [
+      {
+        name: "Moments",
+        list: [
+          " 101 most-wanted gifts",
+
+          "New world workwear",
+
+          "The occasionwear edit",
+
+          "75 getaway get-up",
+        ],
+      },
+
+      {
+        name: "Discover",
+        list: [
+          "150 most-wanted pieces",
+
+          "The bestsellers",
+
+          "Burberry signatures",
+
+          "Off-White icons",
+
+          "The best of Gucci",
+
+          "Conscious",
+
+          "Best of streetwear",
+
+          "Black designers",
+        ],
+      },
+
+      {
+        name: "Botique",
+        list: [
+          "The cult edit",
+
+          "The classic edit",
+
+          "The minimalist edit",
+
+          "The hype streetwear edit",
+        ],
+      },
+    ],
+  },
+
+
+// --------4---------
+
+{
+    id: 4,
+    name: "Clothes",
+    image:'https://cdn-static.farfetch-contents.com/content/images/Home_CMS/Cms/01-06-2022/MW/01-06-MW-CLOTHING-SWIMWEAR.jpg',
+    submenu: [
+      {
+        name: "Activewear",
+        list: [
+            'All activewear',
+
+            'Gym bags & accessories',
+            
+            'Athletic shoes',
+            
+            'Athletic jackets',
+            
+            'Sports tops',
+            
+            'Workout pants',
+            
+            'Athletic shorts',
+            
+            'Skiwear',
+            
+            'Conscious activewear',
+            
+            'Sale activewear',
+        ],
+      },
+
+      {
+        name: "Clothing",
+        list: [
+         
+,'Polo shirts',
+
+'Shirts',
+
+'Shorts',
+
+'Suits',
+
+'Sweaters & knitwear',
+
+'Swimwear',
+
+'Pants',
+
+'T-Shirts & vests',
+
+'Underwear & socks',
+
+'Conscious clothing',
+        ],
+      },
+
+      {
+        name: "Owner",
+        list: [
+            'All pre-owned',
+
+            'Jackets',
+            
+            'Coats',
+            
+            'Tops',
+        ],
+      },
+    ],
+  },
+
+
+
+
+
+];
+
+// export default { arr };
